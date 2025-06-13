9527a44bbb2f29b53395e4396f332eff628522cf
NirwanNeha <nehanirwan1298@gmail.com>
2025-06-13 14:49:20 +0530

    modules required for calender

---
+++ app.module.ts
@@ -0,0 +1,19 @@
+import { DropdownModule } from 'primeng/dropdown';
+import { DialogModule } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+// import { DragDropModule } from 'primeng/dragdrop';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { CalendarModule } from 'primeng/calendar';
+
+// will modify this on production
+imports:[
+    CommonModule,
+    FormsModule,
+    DropdownModule,
+    DialogModule,
+    ButtonModule,
+    TableModule,
+    DragDropModule,
+    OverlayPanelModule,
+    CalendarModule,]
